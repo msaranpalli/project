a53003a2bbff1913e992ab0e9daafbcd32273230
Admin <Admin@Admin-PC>
2017-03-15 21:23:39 +0530

    Print method

---
+++ src/Acess/Mohan.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Acess;
+
+import Acessmodifers.Demo1;
+
+/**
+ *
+ * @author Admin
+ */
+public class Mohan extends Demo1 {
+    void s(){
+        int c = 0;
+        System.out.println(c);
+         System.out.println("ooo");
+    }
+  
+    
+    }
+    
+
